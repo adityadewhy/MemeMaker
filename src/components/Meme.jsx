c6588db22a22243239acc4d08925dd45480c4bc8
aditya <adityadewhy@gmail.com>
2023-10-19 00:06:32 +0530

    handle change and meme text formatting remaining

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 
 export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText:"",
+        bottomText:"",
+        memeImage:"http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                memeImage:url
+            }
+        })
+    }
+
+
 
     
     return (
@@ -24,8 +50,12 @@ export default function Meme() {
                 <button className="form-btn">
                     Get random meme
                 </button>
+            </div>
 
-
+            <div className="meme">
+                <img src={meme.memeImage} className="meme-img" />
+                <h2 className="memeText img-top">{meme.topText}</h2>
+                <h2 className="memeText img-bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
